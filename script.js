60234cc8dfb134702110d9a09062addd9ddbcd3f
El pato feli <87341239+SrCrowbar@users.noreply.github.com>
2023-10-21 22:19:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sample JavaScript for loading and displaying games
+document.addEventListener("DOMContentLoaded", function () {
+    const gamesSection = document.getElementById("games");
+
+    // Replace this with the code to load and display your flash games
+    // For HTML5 games, you can use iframe tags or other methods.
+});
